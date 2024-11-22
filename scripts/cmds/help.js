713ebe2b7057e45d3f3f11b2502f7919fdf6c635
🍒)⎯⃝𝐌𝐎𝐇𝐀𝐌𝐌𝐀𝐃 𝐁𝐀𝐘𝐉𝐈𝐃♡༏༏"🩷🐰༉༎🫰🫶 <130752960+BAYJID404@users.noreply.github.com>
2024-11-22 23:20:10 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🐐 | Goat Bot V2 ]";
+const doNotDelete = "[ 🇧🇩🇧 | BAYJID BOT V 2.5 ]";
 /**
 * @author NTKhang
 * @author: do not delete it
